6cf1f3e20af75e5b893abda86c9cf592c19c7b04
Tormenator1 <shredderofwheat@gmail.com>
2023-11-04 13:15:33 -0400

    Functioning DB
    DB works now,stores all the details google login gives us.

--- Backend/databasehandler.py
+++ Backend/databasehandler.py
@@ -6,7 +6,7 @@ import os
 import MySQLdb
 
 
-def commit_token_email(token, email):
+def commit_token_email(access_token,expires_in,scope,id_token,expires_at, email):
     # Connect to the database
     try:
         connection = MySQLdb.connect(
@@ -23,10 +23,10 @@ def commit_token_email(token, email):
 
         cursor = connection.cursor()
         create_user_query = '''
-                INSERT INTO users (token, email)
-                VALUES (%s, %s)
+                INSERT INTO users (access_token, expires_in, scope, id_token, expires_at, email)
+                VALUES (%s, %s, %s, %s, %s, %s)
             '''
-        cursor.execute(create_user_query,(token,email))
+        cursor.execute(create_user_query,(access_token,expires_in,scope,id_token,expires_at,email))
         connection.commit()
 
         cursor.close()
@@ -36,24 +36,67 @@ def commit_token_email(token, email):
         print(f"Error committing to database: {e}")
         return False
     
-
-
-
-
-
 def main():
-    
+    connection = MySQLdb.connect(
+        host=os.getenv("DATABASE_HOST"),
+        user=os.getenv("DATABASE_USERNAME"),
+        passwd=os.getenv("DATABASE_PASSWORD"),
+        db=os.getenv("DATABASE"),
+        autocommit=True,
+        ssl_mode="VERIFY_IDENTITY",
+        # See https://planetscale.com/docs/concepts/secure-connections#ca-root-configuration
+        # to determine the path to your operating systems certificate file.
+        ssl={ "ca": r"Backend\cacert-2023-08-22.pem"}
+    )
+
     cursor = connection.cursor()
+
+    select = '''SELECT * FROM users'''
+
+    cursor.execute(select)
+
+    results = cursor.fetchall()
+
+    for x in results:
+        print(x)
+    
+    cursor.close()
+
+    connection.close()
+
+
+
+
+def refactor():
+    connection = MySQLdb.connect(
+        host=os.getenv("DATABASE_HOST"),
+        user=os.getenv("DATABASE_USERNAME"),
+        passwd=os.getenv("DATABASE_PASSWORD"),
+        db=os.getenv("DATABASE"),
+        autocommit=True,
+        ssl_mode="VERIFY_IDENTITY",
+        # See https://planetscale.com/docs/concepts/secure-connections#ca-root-configuration
+        # to determine the path to your operating systems certificate file.
+        ssl={ "ca": r"Backend\cacert-2023-08-22.pem"}
+    )
+
+    cursor = connection.cursor()
+    delete_old = '''DROP TABLE users'''
+    cursor.execute(delete_old)
 
     create_table_query = '''
         CREATE TABLE users (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            token VARCHAR(255) NOT NULL,
+            access_token VARCHAR(255) NOT NULL,
+            expires_in INT NOT NULL,
+            scope VARCHAR(255) NOT NULL,
+            id_token VARCHAR(1200) NOT NULL,
+            expires_at INT NOT NULL,
             email VARCHAR(255) NOT NULL
         )
         '''
     
-    #cursor.execute(create_table_query)
+    cursor.execute(create_table_query)
     
     #connection.commit()
     #connection.close()
