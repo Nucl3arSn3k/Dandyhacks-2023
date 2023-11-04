d375becd4003f294990a817f965c10b0d59a9e3d
Kayla Lin <klin23@u.rochester.edu>
2023-11-04 01:48:17 -0400

    added raft component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Box, Button, Flex, Heading, VStack } from "@chakra-ui/react";
 import { BoldedHeader } from "@/components/BoldedHeader";
-import ProgressBar from "@/components/progressBar";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { BobUpAndDown } from "@/components/BobUpAndDown";
@@ -15,46 +14,10 @@ export default function Home() {
           <BoldedHeader text="Name it Here!" fontSize="8em" shadowOffset={8} />
         </BobUpAndDown>
         <Button>Start</Button>
-        <ProgressBar percentage={50} />
       </VStack>
       <Box pos="absolute" top="80%">
         <Raft />
       </Box>
-      {/* <BobUpAndDown
-        position="absolute"
-        bottom="15%"
-        animate={{ translateY: [0, 1, -1, 2, 0] }}
-        zIndex={5}
-      >
-        <Image
-          src="/assets/characters/dolphin.png"
-          alt="Background"
-          width="0"
-          height="0"
-          sizes="100vw"
-          style={{
-            width: "115px",
-            height: "100px",
-          }}
-        />
-      </BobUpAndDown>
-      <BobUpAndDown
-        position="absolute"
-        bottom="10%"
-        animate={{ translateY: [0, 1, -1, 2, 0] }}
-      >
-        <Image
-          src="/assets/raft/raft.png"
-          alt="Background"
-          width="0"
-          height="0"
-          sizes="100vw"
-          style={{
-            width: "300px",
-            height: "100px",
-          }}
-        />
-      </BobUpAndDown> */}
       <Image
         src="/assets/background/ocean_bg.png"
         alt="Background"
