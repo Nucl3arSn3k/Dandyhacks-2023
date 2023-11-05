c56dce46561f40098af86fc62654c2a619d41893
Jassem <toumijassem@gmail.com>
2023-11-05 01:43:06 -0400

    added sound

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,36 @@
 // pages/_app.js
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import React, { useState, useEffect, use } from "react";
+import BackgroundMusic from "@/components/BackgroundMusic";
+import { Howl, Howler } from "howler";
 
 function App({ Component, pageProps }: AppProps) {
+  let playSound = false;
+
+  const sound = new Howl({
+    src: ["assets/sounds/BackgroundMusic.mp3"],
+    loop: false,
+    volume: 0.5,
+    onend: function () {
+      console.log("Finished!");
+    },
+  });
+
+  const onPlaySound = () => {
+    console.log(sound);
+    if (!playSound) {
+      sound.play();
+    } else {
+      sound.pause();
+    }
+    playSound = !playSound;
+  };
+
   return (
     <ChakraProvider>
+      <Button onClick={onPlaySound}>Play</Button>
       <Head>
         <link
           rel="icon"
