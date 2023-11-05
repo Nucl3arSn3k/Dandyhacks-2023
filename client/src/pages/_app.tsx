faaecd9536f6311e1aa06de4bd416fbfc92d4241
Jassem <toumijassem@gmail.com>
2023-11-05 02:01:25 -0500

    fixed sound

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,37 +1,19 @@
-// pages/_app.js
 import { Button, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import React, { useState, useEffect, use } from "react";
+import React from "react";
 import BackgroundMusic from "@/components/BackgroundMusic";
-import { Howl, Howler } from "howler";
+
+
 
 function App({ Component, pageProps }: AppProps) {
-  let playSound = false;
-
-  const sound = new Howl({
-    src: ["assets/sounds/BackgroundMusic.mp3"],
-    loop: false,
-    volume: 0.5,
-    onend: function () {
-      console.log("Finished!");
-    },
-  });
-
-  const onPlaySound = () => {
-    console.log(sound);
-    if (!playSound) {
-      sound.play();
-    } else {
-      sound.pause();
-    }
-    playSound = !playSound;
-  };
-
   return (
     <ChakraProvider>
-      <Button onClick={onPlaySound}>Play</Button>
+
+      <BackgroundMusic />
+
       <Head>
+
         <link
           rel="icon"
           type="image/png"
