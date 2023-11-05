98f48d9ba2a1681b87b409e9676ba054e17ae09e
Kayla Lin <klin23@u.rochester.edu>
2023-11-05 01:25:18 -0400

    get quests

--- client/src/pages/api/getQuests.ts
+++ client/src/pages/api/getQuests.ts
@@ -1,31 +1,31 @@
-// import { getSession } from "next-auth/react";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const session = await getSession({ req });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
 
-//   if (!session) {
-//     res.status(401).json({ error: "You are not authenticated" });
-//     return;
-//   }
+  if (!session) {
+    res.status(401).json({ error: "You are not authenticated" });
+    return;
+  }
 
-//   const userId = session.user.e// Assuming your user object has an "id" field
-
-//   try {
-//     const quests = await prisma.quest.findMany({
-//       where: {
-//         userId: userId,
-//       },
-//     });
-
-//     res.status(200).json(quests);
-//   } catch (error) {
-//     res.status(500).json({ error: "An error occurred while fetching quests." });
-//   }
-// }
+  if (session.user?.email) {
+    res.status(500).json({ error: "An error occurred while fetching quests." });
+  }
+  try {
+    const quests = await prisma.quest.findMany({
+      where: {
+        userEmail: session.user!.email!,
+      },
+    });
+    res.status(200).json(quests);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching quests." });
+  }
+}
