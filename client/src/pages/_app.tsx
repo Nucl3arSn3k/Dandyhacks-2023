7112335c186e3335630d5a0042f7ff5b1bdbd35d
Jassem <toumijassem@gmail.com>
2023-11-04 23:27:14 -0400

    added Favicon

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,20 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/android-chrome-512x512.png"
+        />
+      </Head>
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
