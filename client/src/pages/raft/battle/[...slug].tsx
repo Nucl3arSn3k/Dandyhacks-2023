b55da6bd7c4f90b1c3dcc2f33ac4589184186045
Kayla Lin <klin23@u.rochester.edu>
2023-11-05 03:16:56 -0500

    routes

--- client/src/pages/raft/battle/[...slug].tsx
+++ client/src/pages/raft/battle/[...slug].tsx
@@ -1,146 +1,49 @@
-import { BoldedHeader } from "@/components/BoldedHeader";
-import { StonesContainer } from "@/components/StonesContainer";
-import { HStack, VStack } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import Chat from "@/components/Chat";
-import InputStone from "@/components/InputStone";
-import { StonesButton } from "@/components/StonesButton";
-import { BobUpAndDown } from "@/components/BobUpAndDown";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { QUESTS } from "@/consts";
-import { useSession } from "next-auth/react";
-import { PrismaClient, Prisma } from "@prisma/client";
-import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const prisma = new PrismaClient();
 
-const ChatBattle = async ({ task = "biology", messages }) => {
-  // let [val, setValue] = React.useState("");
-  // let [data, setData] = React.useState(messages);
-  const router = useRouter();
-  const questId = router.query.slug;
+interface CreateQuestData {
+  questId: string;
+}
 
-  useEffect(() => {}, []);
+export default async function handleUpdate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data: CreateQuestData = req.body;
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).json({ error: "You are not authenticated" });
+    return;
+  }
+  if (!session.user?.email) {
+    res.status(500).json({ error: "Could not find user email" });
+  }
 
-  const chatEndRef = React.useRef<HTMLDivElement | null>(null);
+  try {
+    const questId = data.questId;
 
-  let newMsges = [];
+    // get question on load
+    const quest = await prisma.quest.findFirst({
+      where: {
+        id: questId,
+        userEmail: session.user!.email!,
+      },
+    });
 
-  const scrollToBottom = () => {
-    if (chatEndRef.current) {
-      chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+    // Create api response for user
+    const questions = await prisma.questMessage.findMany({
+      where: {
+        questId: questId,
+        userEmail: session.user!.email!,
+      },
+    });
 
-  React.useEffect(() => {
-    scrollToBottom();
-  }, [data]);
-
-  return (
-    <VStack pos="relative" h="100vh" overflow="clip" justify="center">
-      <StonesContainer
-        height={"120vh"}
-        width={"110vw"}
-        style={{ marginTop: "5rem" }}
-        stone="stone2"
-      >
-        <VStack width="85%" height="105vh">
-          <VStack p={5} style={{ marginTop: "0" }}>
-            <HStack>
-              <Link
-                href="/raft"
-                onClick={() =>
-                  axios
-                    .post("/api/updateQuest", { msgData: data, questId })
-                    .then((res) => console.log(res))
-                }
-              >
-                <StonesButton
-                  stone="stone7"
-                  width={"25rem"}
-                  headerProps={{ shadowOffset: 3 }}
-                  boxProps={{
-                    position: "absolute",
-                    left: 0,
-                  }}
-                >
-                  Leave
-                </StonesButton>
-              </Link>
-              <BobUpAndDown>
-                <BoldedHeader
-                  fontSize="2.5em"
-                  shadowOffset={4}
-                  py="20px"
-                  marginTop={"2rem"}
-                >
-                  Battle of {data.title}
-                </BoldedHeader>
-              </BobUpAndDown>
-            </HStack>
-          </VStack>
-          <VStack
-            width="100%"
-            height="75%"
-            justifyContent={"space-between"}
-            maxW="5xl"
-          >
-            <VStack
-              width="100%"
-              maxHeight="80%"
-              overflow="hidden"
-              overflowY="scroll"
-              py={5}
-              css={{
-                "&::-webkit-scrollbar": {
-                  width: "4px",
-                },
-                "&::-webkit-scrollbar-track": {
-                  width: "6px",
-                },
-                "&::-webkit-scrollbar-thumb": {
-                  background: "#564B4B",
-                  borderRadius: "24px",
-                },
-              }}
-              gap="1rem"
-            >
-              {data.conversation.map((msg, idx) => (
-                <Chat key={idx} msg={msg.msg} from={msg.from} />
-              ))}
-              <div ref={chatEndRef} />
-            </VStack>
-            <InputStone
-              onChange={(e) => setValue(e.target.value)}
-              val={val}
-              onEnter={() => {
-                if (val.trim() == "") {
-                  return;
-                }
-                // call AI here
-                return setData((data) => {
-                  setValue("");
-                  newMsges.push({
-                    msg: val,
-                    timestamp: new Date(),
-                    from: "user",
-                  });
-                  return {
-                    ...data,
-                    conversation: [
-                      ...data.conversation,
-                      { msg: val, from: "user" },
-                    ],
-                  };
-                });
-              }}
-            />
-          </VStack>
-        </VStack>
-      </StonesContainer>
-    </VStack>
-  );
-};
-
-export default ChatBattle;
+    res.status(201).json(questions);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching quests." });
+  }
+}
